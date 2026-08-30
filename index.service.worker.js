e08603bca276e88ca90236d4ac17cddcc28d8bfc
Primož Žnidar <sg.prima@gmail.com>
2026-08-30 10:38:40 +0200

    Show menu version and Get Latest refresh.
    Web mobile export with a top-right build version and a Get Latest button that clears the Safari PWA cache.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788038137|831080';
+const CACHE_VERSION = '1788079082|793807';
 /** @type {string} */
 const CACHE_PREFIX = 'sheepdog-lite-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
