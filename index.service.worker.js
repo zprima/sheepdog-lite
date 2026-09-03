ca8fb423642422d4e6cb5dc4abe10292b58631c7
Primož Žnidar <sg.prima@gmail.com>
2026-09-03 15:38:23 +0200

    Update web export

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788083726|878364';
+const CACHE_VERSION = '1788442568|1137459';
 /** @type {string} */
 const CACHE_PREFIX = 'sheepdog-lite-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
