6012ba30fa71f2bb60113fa6005accbe9f7495a1
Primož Žnidar <sg.prima@gmail.com>
2026-09-06 11:38:07 +0200

    Deploy web export v0.1.7

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788624050|1040575';
+const CACHE_VERSION = '1788687464|1025838';
 /** @type {string} */
 const CACHE_PREFIX = 'sheepdog-lite-sw-cache-';
 // Sheepdog web update policy v2
